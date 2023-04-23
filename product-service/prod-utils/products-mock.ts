6fc5e258604e4904bf252c0db4e6622cd161489b
oldjunior <anton.b.utkin@gmail.com>
2023-04-23 15:21:41 +0300

    Module 4 tasks: Integration with NoSQL Database
    * remove ids from products mock

--- product-service/prod-utils/products-mock.ts
+++ product-service/prod-utils/products-mock.ts
@@ -1,27 +1,23 @@
 exports.productsMock = [
   {
-    id: '001',
     title: 'Plumbus',
     count: 579,
     description: 'All-purpose home device.',
     price: 6.5
   },
   {
-    id: '002',
     title: 'Fleeb',
     count: 135,
     description: 'The tool for making Plumbuses.',
     price: 2
   },
   {
-    id: '003',
     title: 'Dinglebop',
     count: 357,
     description: 'The basic material Plumbuses are made of.',
     price: 1
   },
   {
-    id: '004',
     title: 'Schleem',
     count: 468,
     description: 'The material used in the process of making Plumbuses.',
